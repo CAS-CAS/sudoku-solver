20ff45f168e1cda54de8f0d754e3249fae56843c
CAS-CAS <akiracas4108@gmail.com>
2018-03-01 21:56:04 +0900

    コメント改善

--- sudoku_solver.ml
+++ sudoku_solver.ml
@@ -1,4 +1,5 @@
 exception Dpll_error;;
+
 (*
  * 同じ3*3ブロックに属するマスのリスト
  *)
@@ -244,6 +245,8 @@ let rec change_decided_mass num_decided n index sudoku_ls =
      else x::(change_decided_mass num_decided n (index+1) tl)
   | _ -> failwith "error cdm";;
 
+
+
 (*
  * sudoku_ls : 現在の数独候補リスト
  * mass_ls   : 検証しているマスの入る数字の候補リスト  
@@ -262,6 +265,15 @@ let rec check_board n mass_ls sudoku_ls=
   | _ -> failwith "error c b";;
 
 
+
+(*
+ * sudoku_ls     : 数独候補リストの前から(n-1)番目以降
+ * sudoku_origin : 元々の数独候補リスト  
+ * n             : 見ているマスのインデックス
+ *
+ * 候補が複数あるマスを適当に数字を確定させた数独候補リストと、
+ * 仮確定する前の候補から選んだ数字を候補からのぞいた数独候補リストの組を返す。
+ *)
 let rec dpll n sudoku_ls sudoku_origin =
   match sudoku_ls with
   | []    -> ([],[])
@@ -282,64 +294,23 @@ let rec dpll n sudoku_ls sudoku_origin =
 
 
 (*
- * sudoku_ls : 現在の数独候補リスト
- * n         : 参照しているマスのインデックス
+ * sudoku_ls     : 現在の数独候補リスト
+ * n             : 参照しているマスのインデックス
+ * sudoku_before : n = 0 時点での数独候補リスト
+ * stack         : マスを仮確定した際に、仮確定マスの数独候補リストを格納するスタック
  *
  * 参照しているマスによって動作を変更する。
  * マスに入る数字が確定しているときは、そのマスの行・列・ブロックに属するマスから、
  * 確定している数字を候補から除去する。
+ * 
  * マスに入る数字が確定していない場合は、そのマスの候補数字を検証して、
  * 同じブロックに属するマスの候補にない場合、数字を確定する。
- *)
-
-
-(*
-print_board 0 sudoku_dpll;
-         print_string "\n";
- *)
-(*
-let rec check n sudoku_ls sudoku_before stack =
-  if (check_complete sudoku_ls)
-  then print_board 0 sudoku_ls
-  else
-    if n > 80 then
-      if sudoku_ls = sudoku_before then
-        (
-         let (sudoku_dpll, sudoku_stack) = (dpll 0 sudoku_ls) in
-         check 0 sudoku_dpll sudoku_dpll (sudoku_stack::stack);
-         
-        )
-      else check 0 sudoku_ls sudoku_ls stack
-    else
-      
-      let mass_ls = (List.nth sudoku_ls n) in
-      begin 
-        if (List.length mass_ls) = 1
-        then let num_decided = (List.hd mass_ls) in
-             let sudoku_ls2 =
-               try (check_width  num_decided n 0 sudoku_ls)
-               with Dpll_error -> []
-             in
-             let sudoku_ls3 =
-               try (check_height num_decided n 0 sudoku_ls2)
-               with Dpll_error -> []
-             in
-             let sudoku_ls4 =
-               try (check_block  num_decided n 0 sudoku_ls3)
-               with Dpll_error -> []
-             in
-             if sudoku_ls4 <> []
-             then check (n+1) sudoku_ls4 sudoku_before stack
-             else
-               match stack with
-               | x::tl -> check 0 x x tl
-               | _     -> failwith "error"
-                        
-        else let sudoku_ls2 = (check_board n mass_ls sudoku_ls) in
-             if sudoku_ls = sudoku_ls2
-             then check (n+1) sudoku_ls2 sudoku_before stack
-             else check n sudoku_ls2 sudoku_before stack
-      end;;
+ * 
+ * 上記の方法ではsudoku_lsに変更がなくなった場合、マス一つを指定して、
+ * そのマスの候補から一つを仮確定して、続ける。
+ * その際、仮確定する前にstackに、指定したマスの候補リストから、
+ * 仮確定した数字を候補から外したリスト仮確定する前の数独候補リストをストックしておく。
+ * もし、仮確定が失敗したとわかったとき、この数独候補リストから再開することができる。
  *)
 
 let rec check n sudoku_ls sudoku_before stack=
@@ -408,6 +379,7 @@ let sudoku =
     ;0;6;0; 0;7;0; 0;8;0
     ;2;0;0; 0;0;0; 4;0;0];;
  *)
+
 (*
 let sudoku = 
   [  0;2;0; 0;0;0; 0;0;9
@@ -422,6 +394,8 @@ let sudoku =
     ;1;3;0; 0;0;8; 0;9;0
     ;0;0;0; 0;0;0; 0;0;5];;
  *)
+
+
 let sudoku = 
   [  0;0;0; 0;0;0; 0;0;0
     ;0;0;0; 0;0;1; 0;8;0
@@ -435,6 +409,7 @@ let sudoku =
     ;7;0;0; 0;6;0; 0;0;0
     ;0;0;0; 0;2;0; 0;0;0];;
 
+(*
 let sudoku = 
   [  0;3;0; 0;9;0; 0;1;0
     ;1;0;9; 0;8;0; 0;0;6
@@ -447,13 +422,13 @@ let sudoku =
     ;0;0;7; 6;0;1; 0;0;8
     ;0;0;2; 0;0;0; 0;3;4
     ;0;0;0; 0;0;0; 9;0;0];;
- 
+ *) 
 
 (*
  * 入力として与えられた数独を、
  * すでにわかっているマスはその数字のみが入ったリスト、
  * 
-わかっていないマスは0~9が格納されたリストに変えたint list list(数独候補リスト) を作成する。
+わかっていないマスは0~9が格納されたリストに変えた数独候補リストを作成する。
  *)
 let sudoku_list =
   (List.map (fun x -> if x = 0 then [1;2;3;4;5;6;7;8;9] else [x]) sudoku);;
